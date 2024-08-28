b66373a972719c6a0b2234b311f91b1909898bbd
Md Nasir <archimidis@rocketmail.com>
2024-08-28 14:52:38 +0800

    Update getTrip.php

--- v1/getTrip.php
+++ v1/getTrip.php
@@ -1,29 +1,28 @@
 <?php
-    // ini_set('display_errors', '1');
-    // ini_set('display_startup_errors', '1');
-    // error_reporting(E_ALL);
-    
-    include("validate.php");
-    include("../database.php");
-    
-    $conn = connect_database();
-    
-    $headers = getallheaders();
-    $valid = validate($headers["Appkey"]);
-    
-    $response = array();
-    $response["result"] = "Invalid request..";
-    http_response_code(400);
-    
-    $arr_truck_id = array();
-    $arr_truck_plate = array();
-    
-    if($valid==1)
-    {
-        if ($_SERVER['REQUEST_METHOD'] === 'GET') 
-        {
-            $user = htmlspecialchars($_GET["user"], ENT_QUOTES);
-            
+	// ini_set('display_errors', '1');
+	// ini_set('display_startup_errors', '1');
+	// error_reporting(E_ALL);
+	
+	include("validate.php");
+	include("../database.php");
+	
+	$conn = connect_database();
+	
+	$headers = getallheaders();
+	$valid = validate($headers["Appkey"]);
+	
+	$response = array();
+	$response["result"] = "Invalid request..";
+	http_response_code(400);
+	
+	$arr_truck_id = array();
+	$arr_truck_plate = array();
+	
+	if($valid==1)
+	{
+		if ($_SERVER['REQUEST_METHOD'] === 'GET') 
+		{
+			$user = htmlspecialchars($_GET["user"], ENT_QUOTES);
 			$sql = "select * from db_table where username='".$user."' ";
 			$res = mysqli_query($conn, $sql);
 			if(mysqli_num_rows($res)>0)
@@ -38,12 +37,11 @@
 				}
 				$arr_response["count"] = count($arr_response_temp);
 				$arr_response["data"] = $arr_response_temp;
-				
 				$arr_response["result"] = "Successful..";
 				http_response_code(200);
 			}
-        }
-    }
-    
-    header('Content-Type: application/json');
-    echo json_encode($arr_response);
+		}
+	}
+	
+	header('Content-Type: application/json');
+	echo json_encode($arr_response);
